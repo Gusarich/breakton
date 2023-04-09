97f44c90673a90133922861ae44ce2a170c07f37
Gusarich <danielsedovzzz@gmail.com>
2023-04-09 20:29:52 +0300

    update script

--- scripts/send100Messages.ts
+++ scripts/send100Messages.ts
@@ -3,14 +3,19 @@ import { Flooder } from '../wrappers/Flooder';
 import { compile, NetworkProvider } from '@ton-community/blueprint';
 import { getSecureRandomBytes, keyPairFromSeed } from 'ton-crypto';
 
+const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
 export async function run(provider: NetworkProvider) {
     const keypair = keyPairFromSeed(await getSecureRandomBytes(32));
 
     const flooder = provider.open(Flooder.createFromConfig({ publicKey: keypair.publicKey }, await compile('Flooder')));
 
-    await flooder.sendDeploy(provider.sender(), toNano('0.25'));
+    await flooder.sendDeploy(provider.sender(), toNano('0.33'));
 
     await provider.waitForDeploy(flooder.address);
 
-    console.log('Secret key to use this flooder:', keypair.secretKey.toString('hex'));
+    for (let i = 0; i < 100; i++) {
+        await flooder.sendMessage(i, keypair);
+        await sleep(100);
+    }
 }
